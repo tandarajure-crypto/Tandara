cc2bcead627d1d82366841140b6a2468066511a8
JURE TANDARA <287090835+tandarajure-crypto@users.noreply.github.com>
2026-08-26 02:31:30 +0200

    Delete js/common directory

--- js/common/branch-page-controls.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-
-  document.querySelectorAll('[data-print-page]').forEach(function (button) {
-    button.addEventListener('click', function () {
-      window.print();
-    });
-  });
-})();
--- js/common/print-diagram.js
+++
@@ -1,4 +0,0 @@
-(function () {
-  var printButton = document.getElementById('printDiagram');
-  if (printButton) printButton.addEventListener('click', function () { window.print(); });
-}());
