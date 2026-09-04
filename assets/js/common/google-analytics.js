2f5acb2ff2376d7a92a468a96b27e28f648588a2
JURE TANDARA <287090835+tandarajure-crypto@users.noreply.github.com>
2026-09-04 05:05:36 -0700

    Add files via upload

--- assets/js/common/google-analytics.js
+++ assets/js/common/google-analytics.js
@@ -2,17 +2,37 @@
   "use strict";
   var GA_ID = "G-S33NBEJQFD";
 
-  // A diagram loaded inside an iframe belongs to the host page view.
-  // If this HTML is opened directly, it is still tracked normally.
-  if (window.self !== window.top) return;
+  function startTracking() {
+    document.querySelectorAll("img[data-flag-counter-src]").forEach(function (image) {
+      image.loading = "eager";
+      image.src = image.getAttribute("data-flag-counter-src");
+      image.removeAttribute("data-flag-counter-src");
+    });
 
-  window.dataLayer = window.dataLayer || [];
-  window.gtag = window.gtag || function () { window.dataLayer.push(arguments); };
-  window.gtag("js", new Date());
-  window.gtag("config", GA_ID);
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = window.gtag || function () { window.dataLayer.push(arguments); };
+    window.gtag("js", new Date());
+    window.gtag("config", GA_ID);
 
-  var script = document.createElement("script");
-  script.async = true;
-  script.src = "https://www.googletagmanager.com/gtag/js?id=" + encodeURIComponent(GA_ID);
-  document.head.appendChild(script);
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=" + encodeURIComponent(GA_ID);
+    document.head.appendChild(script);
+  }
+
+  function scheduleTracking() {
+    if ("requestAnimationFrame" in window) {
+      window.requestAnimationFrame(function () {
+        window.requestAnimationFrame(startTracking);
+      });
+    } else {
+      window.setTimeout(startTracking, 0);
+    }
+  }
+
+  if (document.readyState === "complete") {
+    scheduleTracking();
+  } else {
+    window.addEventListener("load", scheduleTracking, { once: true });
+  }
 })();
